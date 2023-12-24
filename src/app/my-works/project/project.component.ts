54a754ba37e79bf833714156046697195ef27bd9
SolAndrade <solnicandrade@gmail.com>
2023-12-24 16:48:00 +0100

    [PROJECTS] nav animation and txt display

--- src/app/my-works/project/project.component.ts
+++ src/app/my-works/project/project.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WorksService } from 'src/app/services/works.service';
@@ -5,18 +6,33 @@ import { WorksService } from 'src/app/services/works.service';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  animations: [
+    trigger('navbarAnimation', [
+      state('visible', style({
+        opacity: 1,
+      })),
+      state('hidden', style({
+        opacity: 0,
+      })),
+      transition('visible => hidden', animate('600ms ease-out')),
+      transition('hidden => visible', animate('300ms ease-in')),
+    ]),
+  ],
 })
 export class ProjectComponent implements OnInit {
+  navbarState = 'visible';
   project: any;
   currentImageIndex = 0;
   showMainInfo = true;
+  activeRightButton = true;
+  activeLeftButton = false;
+  projectDescriptionTxt = false;
 
 
   constructor(
     private route: ActivatedRoute,
     private worksService: WorksService,
-    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -29,12 +45,16 @@ export class ProjectComponent implements OnInit {
   }
 
   changeImage(index: number) {
+    const isVisible = this.navbarState === 'visible';
+    const isHidden = this.navbarState === 'hidden';
+    (index === 1 && isVisible) || (index === -1 && isHidden) ? this.toggleNavbarAnimation() : null;
     this.currentImageIndex = (this.currentImageIndex + index + this.project.images.length) % this.project.images.length;
-    if(this.currentImageIndex === 0) {
-      this.showMainInfo = true;
-    } else {
-      this.showMainInfo = false;
-    }
+    this.showMainInfo = this.currentImageIndex === 0;
+    this.projectDescriptionTxt = this.currentImageIndex === 1;
+  }
+
+  toggleNavbarAnimation() {
+    this.navbarState = (this.navbarState === 'visible') ? 'hidden' : 'visible';
   }
 
 }
