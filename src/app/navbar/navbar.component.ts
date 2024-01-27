8224475e6b0ed799ca4d280e25712cb4946e9e25
SolAndrade <solnicandrade@gmail.com>
2024-01-27 16:57:45 +0100

    contact page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,21 +8,36 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   isNavItemHovered = false;
+  isHovered: boolean = false;
 
-  onNavItemHover(itemId: string): void {
+  constructor(private router: Router) {}
+
+
+  onHover(): void {
+    this.isHovered = true;
+  }
+
+  onLeave(): void {
+    this.isHovered = false;
+  }
+
+  /*onNavItemHover(itemId: string): void {
     const hoveredItem = document.getElementById(itemId);
     const nav = document.getElementById('nav');
 
     if (hoveredItem && nav) {
       nav.classList.add('background');
+      setTimeout(() => {
+        this.router.navigate([`/${itemId}`]);
+      }, 1000);
     }
-  }
+  }*/
 
-  handleUnhover(): void {
+  /*handleUnhover(): void {
     const nav = document.getElementById('nav');
     
     if (nav) {
       nav.classList.remove('background');
     }
-  }
+  }*/
 }
