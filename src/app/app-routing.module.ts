500901e38eb65eca35ce8d95491e80c96afb799b
SolAndrade <solnicandrade@gmail.com>
2024-02-19 16:21:20 +0100

    another routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,13 @@ import { ProjectComponent } from './my-works/project/project.component';
 import { MyWorksComponent } from './my-works/my-works.component';
 
 const routes: Routes = [
-  { path: 'portfolio', component: HomeComponent },
-  { path: '/home', component: HomeComponent },
-  { path: '/works', component: MyWorksComponent },
-  { path: '/about', component: AboutComponent },
-  { path: '/playground', component: PlaygroundComponent },
-  { path: '/contact', component: ContactComponent },
-  { path: '/projects/:id', component: ProjectComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'works', component: MyWorksComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'playground', component: PlaygroundComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects/:id', component: ProjectComponent },
   { path: '', component: HomeComponent },
 ];
 
