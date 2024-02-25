053a151b565fb58510ddccefb0f90a6ca15a27e8
SolAndrade <solnicandrade@gmail.com>
2024-02-25 18:24:45 +0100

    about-me page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  aboutMeImg = './assets/img/about-me.png'
+  aboutMeImg = './assets/img/about-me.png';
+  cv = './assets/data/CV Sol Andrade.pdf';
+  arrowDiagonal = './assets/img/buttons/arrow-diagonal.png';
+  
+  topSkills: string[] = [
+    "WEB DESIGN",
+    "FRONTEND DEVELOPMENT",
+    "GRAPHIC DESIGN",
+    "BRAND DESIGN",
+    "SOFTWARE DEVELOPMENT",
+    "BRAND VISUAL STRATEGY",
+    "APPLICATION DEVELOPMENT",
+    "UX/UI",
+    "INTERACTION DESIGN",
+    "DIGITAL INTERFACE DESIGN",
+    "WEB DEVELOPMENT",
+    "FULLSTACK DEVELOPMENT",
+    "PROGRAMMING",
+  ];
+
+  hardSkills: string[] = [
+    "ANGULAR",
+    "HTML",
+    "TYPESCRIPT",
+    "CSS",
+    "JAVA",
+    "GITHUB",
+    "REACT",
+    "SPRINGBOOT",
+    "JAVASCRIPT",
+    "SCRUM",
+    "ADOBE PHOTOSHOP",
+    "API RESTFUL",
+    "FIGMA",
+    "PYTHON",
+    "TRELLO",
+    "ARDUINO",
+    "ADOBE ILLUSTRATOR",
+    "C",
+    "POSTMAN",
+    "CANVA",
+    "POSTGRESQL",
+    "C#",
+    "UNIT TESTING",
+    "GIT",
+    "AWS INFRASTRUCTURE",
+    "MYSQL",
+    "ADOBE CREATIVE SUITE",
+    "UNITY",
+    "JIRA"
+  ]
+
+  topSkillsDisplay!: string[];
+  hardSkillsDisplay!: string[];
+
+  constructor() {
+    this.topSkillsDisplay = this.topSkills.concat(this.topSkills);
+    this.topSkillsDisplay = this.topSkillsDisplay.concat(this.topSkillsDisplay);
+
+    this.hardSkillsDisplay = this.hardSkills.concat(this.hardSkills);
+  }
 }
